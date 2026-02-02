5b4abbdd6a522918803e7b1a5e4a5ce4628b4255
Max Bisschop <max@ffkijken.com>
2026-02-02 21:18:11 +0100

    check

--- automate/automate.py
+++ automate/automate.py
@@ -25,7 +25,7 @@ from device_controller import AutomateController, ScheduleController, BaseDevice
 # ============================================================================
 
 # Time to pause between loop iterations (seconds)
-LOOP_INTERVAL_SECONDS = 20
+LOOP_INTERVAL_SECONDS = 29
 
 # Time between schedule API refreshes (seconds) - 5 minutes
 API_REFRESH_INTERVAL_SECONDS = 300
